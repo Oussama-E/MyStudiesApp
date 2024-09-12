b5a550cdc9d1f6cbacec7e55375a79e50f995197
Oussama-E <oussamaelbou@protonmail.com>
2024-09-12 13:16:59 +0200

    refactor: add method in StudentController, test passed

--- Student/src/main/java/com/example/student/StudentController.java
+++ Student/src/main/java/com/example/student/StudentController.java
@@ -1,6 +1,10 @@
 package com.example.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,10 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable int id){
+        Student std = studentService.getStudentById(id);
+        if (std==null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else return new ResponseEntity<>(std, HttpStatus.OK);
+    }
 }
