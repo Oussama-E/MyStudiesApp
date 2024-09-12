808ee524ae64284c4d096fa502368b82bf7ee568
Oussama-E <oussamaelbou@protonmail.com>
2024-09-12 15:59:39 +0200

    refactor: getStudents in StudentController -> test passed

--- Student/src/main/java/com/example/student/StudentController.java
+++ Student/src/main/java/com/example/student/StudentController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
@@ -23,5 +25,11 @@ public class StudentController {
         Student std = studentService.getStudentById(id);
         if (std==null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         else return new ResponseEntity<>(std, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Set<Student>> getStudents(){
+        Set<Student> students = studentService.getStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 }
