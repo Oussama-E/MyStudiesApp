3eabafd5e7ff93ac050265b36fd80ead50792af0
Oussama-E <oussamaelbou@protonmail.com>
2024-09-12 16:28:09 +0200

    feat: add a student insert in import.sql + http test method gor all

--- Student/src/main/resources/import.sql
+++ Student/src/main/resources/import.sql
@@ -1,2 +1,3 @@
-INSERT INTO students (id, lastname, firstname, birthdate, grade) VALUES (1, 'Doe', 'John', '2000-01-01', 1);
-INSERT INTO students (id, lastname, firstname, birthdate, grade) VALUES (2, 'Do', 'Ay', '2005-05-05', 2);
+INSERT INTO students (id, lastname, firstname, birthdate, grade) VALUES (1, 'Doe', 'John', '2000-01-01', 3);
+INSERT INTO students (id, lastname, firstname, birthdate, grade) VALUES (2, 'Do', 'Ay', '2001-05-05', 2);
+INSERT INTO students (id, lastname, firstname, birthdate, grade) VALUES (3, 'Does', 'Hee', '2002-02-02', 1);
