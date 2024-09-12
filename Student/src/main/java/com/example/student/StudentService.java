52b9b8df54e5a16b06ece26897ce19762eac91bd
Oussama-E <oussamaelbou@protonmail.com>
2024-09-12 12:57:58 +0200

    feat: add interface repository, controller and service for Student

---
+++ Student/src/main/java/com/example/student/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.student;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    private StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+}
